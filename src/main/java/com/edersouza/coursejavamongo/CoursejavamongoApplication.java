2a55801efcf1d7a1389c3a3ecea6c623c60a306a
Eder Souza <edernorival@gmail.com>
2020-12-20 16:17:16 -0300

    first commit, created project

---
+++ src/main/java/com/edersouza/coursejavamongo/CoursejavamongoApplication.java
@@ -0,0 +1,13 @@
+package com.edersouza.coursejavamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursejavamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursejavamongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edersouza/coursejavamongo/CoursejavamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edersouza.coursejavamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursejavamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
