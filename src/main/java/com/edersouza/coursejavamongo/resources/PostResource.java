04494ea441b9e6aa309b21d96ce7488b7c471746
Eder Souza <edernorival@gmail.com>
2020-12-20 19:24:05 -0300

    Endpoint returning post by Id

---
+++ src/main/java/com/edersouza/coursejavamongo/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.edersouza.coursejavamongo.resources;
+
+import com.edersouza.coursejavamongo.domain.Post;
+import com.edersouza.coursejavamongo.services.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    private final PostService postService;
+
+    public PostResource(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+}
---
+++ src/main/java/com/edersouza/coursejavamongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.edersouza.coursejavamongo.services;
+
+import com.edersouza.coursejavamongo.domain.Post;
+import com.edersouza.coursejavamongo.repository.PostRepository;
+import com.edersouza.coursejavamongo.services.exception.ObjectNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(
+                () -> new ObjectNotFoundException("Object not found"));
+    }
+}
